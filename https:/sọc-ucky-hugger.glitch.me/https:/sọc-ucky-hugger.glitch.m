3d998e5857a7c4eccbd7872c12074194455e4eed
Đinh Văn Thanh <72543449+dinhvanthanh456@users.noreply.github.com>
2021-11-23 20:08:23 +0700

    Create sọc-ucky-hugger.glitch.m

---
+++ https:/sọc-ucky-hugger.glitch.me/https:/sọc-ucky-hugger.glitch.m
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
+    <link rel="icon" href="https://glitch.com/favicon.ico" />
+    <title>phim hay.net</title>
+    <link rel="canonical" href="https://glitch-hello-website.glitch.me/" />
+    
+   </body>
+      <!--  1.  hiển thi nội dung
+2. lấy thông tinh người dùng -->
+      
+    <h1>
+      phim hành động
+      </h1>
+       <h2>
+     1: Sát thủ John Wick - John Wick (2014)
+      </h2>
+    <a href="https://https://www.ssphim.net/">link phim Sát thủ John Wick - John Wick (2014) </a>
+       <h3>
+      <img src="https://www.google.com/search?q=S%C3%A1t+th%E1%BB%A7+John+Wick+-+John+Wick+(2014)&sxsrf=AOaemvKNwhdSVtKHcVMMlWVtWNDAKpgA1w:1637670994930&source=lnms&tbm=isch&sa=X&ved=2ahUKEwiF_6Tvv670AhViIaYKHb_FA4MQ_AUoAnoECAEQBA&biw=1536&bih=754&dpr=1.25#imgrc=YO8X4zBXPVx7BM"/>
+      </h3>
+       <h4>
+    2:Kế hoạch A 2
+      </h4>
+     <a href="https://https://www.youtube.com/watch?v=ymfYOSv7buU">link phim Kế hoạch A 2</a>
+     
+    <h5>
+       <img src="https://www.google.com/search?q=K%E1%BA%BF+ho%E1%BA%A1ch+A+2&sxsrf=AOaemvJwfWTtf9dAs2h-GvZ_qdeOY0GSCQ:1637671536139&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjI7q3xwa70AhWGZd4KHewRAhgQ_AUoAnoECAEQBA&cshid=1637671594308080&biw=1536&bih=754&dpr=1.25#imgrc=VkOv_-NiWWdCuM"/>
+      </h5>
+     <h6>
+    3:Vệ sĩ sát thủ
+      </h6>
+    <a href="https://https://https://vieon.vn/ve-si-sat-thu.html">link phim Vệ sĩ sát thủ </a>
+    <h1>
+      </h1>
+    <img src="https://www.google.com/search?q=V%E1%BB%87+s%C4%A9+s%C3%A1t+th%E1%BB%A7&sxsrf=AOaemvLZ8fK9dLTqHJZqNSJp14ylhGZiyw:1637671848913&source=lnms&tbm=isch&sa=X&ved=2ahUKEwjZi8CGw670AhWHFIgKHUV_AwUQ_AUoAnoECAEQBA&cshid=1637671890769176&biw=1536&bih=754&dpr=1.25#imgrc=L8UhzDNJ9GHIIM" />
+  </body>
+</html>
+  </body>
+</html>
